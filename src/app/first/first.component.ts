7b4d465222e208fccfc928dad02ad452b6873842
Michael Banvard <mjbanvard@gmail.com>
2021-03-05 14:45:01 -0600

    Solution for hiccough with XCode and TypeScript.

--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-first',
   templateUrl: './first.component.html',
-  styleUrls: ['./first.component.css']
+  styleUrls: ['./first.component.scss']
 })
 export class FirstComponent implements OnInit {
 
   text: string = 'My First Component is Ready.';
   numbers: number[] = [29, 14, 70, 51, 33];
-  
+  isGreen: boolean = true;
+  cash: number = 1.99;
+  // date: new Date(){};
+
   constructor() { }
 
   ngOnInit(): void {
